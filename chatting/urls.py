26f66183e5c7b91ec0f33e2c635f8225d2cb7239
seuljaa <seuljaa940503@gmail.com>
2022-04-14 15:37:42 +0900

    room.html 생성

--- chatting/urls.py
+++ chatting/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from chatting import views
 
 urlpatterns = [
-    path('' , views.main , name='main'),
+    path('', views.main, name='main'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
--- chatting/views.py
+++ chatting/views.py
@@ -1,5 +1,15 @@
+import json
+
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils.safestring import mark_safe
+
+
 def main(request):
     return render(request, 'main.html')
+
+def room(request, room_name):
+    return render(request, 'room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name))
+    })
